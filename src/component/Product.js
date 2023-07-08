874a15bcfece12f05f621b1e03dcc255a6d18c15
Roshan Guragain <twistfreaky@gmail.com>
2023-07-08 14:05:41 +0545

    Api imported

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 const Product = () => {
+
+    const [products, setPrdoucts] = useState([])
+    useEffect(()=>
+    {
+        fetch("https://fakestoreapi.com/products")
+        .then(data => data.json())
+        .then(result => setPrdoucts(result))
+    }, [])
   return (
     <div>
         <h1>Product DashBoard</h1>
+        {JSON.stringify(products)}
     </div>
   )
 }
