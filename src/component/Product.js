648355f7bd8a2655a4df9a10cce5adeb49a905cc
Roshan Guragain <twistfreaky@gmail.com>
2023-07-17 10:06:53 +0545

    Product  card added

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react'
-
+import React, { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
+// import ProductCard from "./ProductCard";
 
 const Product = () => {
-
-    const [products, setPrdoucts] = useState([])
-    useEffect(()=>
-    {
-        fetch("https://fakestoreapi.com/products")
-        .then(data => data.json())
-        .then(result => setPrdoucts(result))
-    }, [])
+  const [products, setProdoucts] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((data) => data.json())
+      .then((result) => setProdoucts(result));
+      
+  }, []);
   return (
     <div>
-        <h1>Product DashBoard</h1>
-        {JSON.stringify(products)}
+      <h1>Product DashBoard</h1>
+    
+      <ProductCard  products={products}/>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
